e1b721c08df0842d89026950c1ca5765dfba8a69
Mateusz Wu <15673167+worotyns@users.noreply.github.com>
2024-08-23 11:39:51 +0200

    fix

--- example/molecule_example.ts
+++ example/molecule_example.ts
@@ -29,19 +29,20 @@ metadata.mutate({
 });
 
 await metadata.persist();
-console.log(team.toJSON());
+// console.log(team.toJSON());
 
 // I can directly get from repository and persist
 const repository = createRepository(denoRuntime);
 const user = await repository.atoms.restore<ListAtom>(deserialize("identity::dev:team:xyz:atoms:users"));
 
 assert(user, 'user atom exists in db');
-console.log(user.toJSON());
+// console.log(user.toJSON());
 user.add('third@email.com');
 
-// It's not from molecule so i cannot use .persist() method
+// It's not from molecule so i cannot use .persist() method - now version mismatched
 await repository.atoms.persist(user);
 console.log(user.toJSON())
+console.log(team.toJSON())
 
 // Now when I try to store changes from molecule, then I will get exception due to version
 // teamUsers.add('conflict@email.com')
