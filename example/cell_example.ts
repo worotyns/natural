70a854964478ef4c2591c0e79dbdb579d3775625
Mateusz Wu <15673167+worotyns@users.noreply.github.com>
2024-08-27 07:22:02 +0200

    fix

--- example/cell_example.ts
+++ example/cell_example.ts
@@ -1,106 +1,84 @@
-import { deserialize } from "../identity.ts";
+import { assert } from "../assert.ts";
 import {
-  cell,
-  createRepository,
-  identity,
-  memoryRuntime,
+  type ListAtom,
   type Molecule,
+  type NamespacedIdentity,
+  type NumberAtom,
   temporary,
 } from "../mod.ts";
-import { assert } from "../testing.ts";
 
-async function createUser(name: string) {
-  const user = temporary("dev", "users", name);
-  user.list([], "friends");
-  await user.persist();
-}
-
-// Write some test data
-await createUser("jane@email.com");
-await createUser("joe@email.com");
-
-const repository = createRepository(memoryRuntime);
-
-// Make friend process with acceptance from invited user
-const evolution = cell(identity("make-friends"), async (ctx) => {
-  // Log processing time
-  ctx.log("starting");
-
-  // Fetch args for inviting and invited and assert this
-  const invitingId = ctx.get<string>("inviting");
-  const invitedId = ctx.get<string>("invited");
-
-  // Assert that args was declared via args
-  assert(invitingId, "invited must be defined");
-  assert(invitedId, "inviting must be defined");
-
-  // Fetch invited from DB
-  const invited = await ctx.restore<Molecule>(deserialize(invitedId));
-  const inviting = await ctx.restore<Molecule>(deserialize(invitingId));
-
-  // Assert that data exists
-  assert(invited, "invited must be defined");
-  assert(inviting, "inviting must be defined");
-
-  // Send e-mail with code and link to click
-  await ctx.run(identity("send-invite-fake-email"), async (runCtx) => {
-    await runCtx.set("invited.token", "1234");
-    // sending e-mail fake
-    console.log(
-      invitingId,
-      "Click to accept invitation",
-      `sending email with content: ${invitingId} has invited you to join them click to https://localhost:3000/${ctx.identity.serialize()}/resolve/${
-        runCtx.get("invited.token")
-      }`,
-    );
-    runCtx.log("sent email with code");
-  });
-
-  // Sleep process, when user click process will woke up and continue
-  await ctx.waitFor("email.clicked");
-
-  // After user clicks, then will continue with args passed {'email.token', and 'invited.clicked'}
-  await ctx.run(identity("check-code"), async (runCtx) => {
-    const token = await runCtx.get("email.token");
-    const originalToken = await runCtx.get("invited.token");
-
-    assert(token === originalToken, "invalid token");
-
-    runCtx.log("token is ok, try to add to friends!");
-  });
-
-  // Now we can connect two users
-  await ctx.run(identity("modify-transaction"), async (runCtx) => {
-    const [invitedFriends] = invited.use("friends");
-    const [invitingFriends] = inviting.use("friends");
-
-    invitedFriends.mutate([...invitedFriends.value, invitingId]);
-    invitingFriends.mutate([...invitingFriends.value, invitedId]);
-
-    await runCtx.persist(invited, inviting);
-    runCtx.log(`${invitingId} and ${invitedId} are now friends`);
-  });
-}, repository);
-
-// // Run process with user identifiers
-const result = await evolution({
-  invited: "identity::dev:users:jane@email.com",
-  inviting: "identity::dev:users:joe@email.com",
+const johnAccount = temporary("ns://dev/accounts/john@doe.com").defaults({
+  balance: 100,
+  transactions: []
 });
 
-const result2 = await evolution({
-  invited: "identity::dev:users:jane@email.com",
-  inviting: "identity::dev:users:joe@email.com",
-}, result);
+const janeAccount = temporary("ns://dev/accounts/jane@doe.com").defaults({
+  balance: 200,
+  transactions: []
+});
 
-console.log(result.serialize());
+await johnAccount.persist()
+await janeAccount.persist()
 
-// Simulate woke up with code
-const afterClick = await evolution({
-  "email.clicked": "identity::dev:jane@email.com",
-  "email.token": "1234",
-}, result2);
+const transactions = temporary("ns://dev/transactions");
 
-console.log(afterClick.serialize());
+const doTransfer = transactions.durable("do-transaction", async (ctx) => {
+  await ctx.run("transfer", async () => {
 
-console.log(afterClick.toJSON());
+    const from = ctx.get<NamespacedIdentity>("from");
+    assert(typeof from === "string", "from must be string");
+
+    const to = ctx.get<NamespacedIdentity>("to");
+    assert(typeof to === "string", "to must be string");
+
+    assert(from !== to, 'from and to cannot be same account');
+
+    const amount = ctx.get("amount");
+    assert(typeof amount === "number", "amount must be number");
+
+    const fromMolecule = await ctx.restore<Molecule>(from);
+
+    assert(fromMolecule, "from must be molecule");
+    const [fromBalance, fromTransactions] = fromMolecule.use<
+      [NumberAtom, ListAtom]
+    >("balance", "transactions");
+
+    const toMolecule = await ctx.restore<Molecule>(to);
+    assert(toMolecule, "to must be molecule");
+    const [toBalance, toTransactions] = toMolecule.use<[NumberAtom, ListAtom]>(
+      "balance",
+      "transactions",
+    );
+
+    ctx.log(`before: ${from} balance: ${fromBalance.valueOf()}`)
+    ctx.log(`before: ${to} balance: ${toBalance.valueOf()}`)
+
+    if (fromBalance.value < amount) {
+      throw new Error("not enough money");
+    }
+
+    fromBalance.mutate(fromBalance.value - amount);
+    toBalance.mutate(toBalance.value + amount);
+
+    ctx.log(`after: ${from} balance: ${fromBalance.valueOf()}`)
+    ctx.log(`after: ${to} balance: ${toBalance.valueOf()}`)
+
+    fromTransactions.add(ctx.identity);
+    toTransactions.add(ctx.identity);
+
+    await ctx.persist(fromMolecule, toMolecule); // transaction save
+  });
+});
+
+const hmm = await doTransfer({
+  from: johnAccount.identity,
+  to: janeAccount.identity,
+  amount: 100,
+});
+
+const restoredJane = await janeAccount.restore();
+const restoredJohn = await johnAccount.restore();
+
+console.log(hmm.toJSON({ pretty: true }))
+console.log(restoredJane.toJSON({ pretty: true }))
+console.log(restoredJohn.toJSON({ pretty: true }))
