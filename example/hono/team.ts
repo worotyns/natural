5beca5ac0b6c0d989490f2136249ac6860842f86
Mateusz Wu <15673167+worotyns@users.noreply.github.com>
2024-08-29 20:48:36 +0200

    fix

--- example/hono/team.ts
+++ example/hono/team.ts
@@ -1,10 +1,26 @@
+import type { NamespacedIdentity } from "../../mod.ts";
+
 interface Meta {
   createdAt: number;
   updatedAt: number;
   deletedAt: number;
 }
 
+interface MemberMeta {
+  state: "invited" | "joined"
+  joinedAt: number
+  invitedBy: NamespacedIdentity
+}
+
+interface Member {
+  role: number;
+  name: string;
+  nsid: NamespacedIdentity;
+  meta: MemberMeta
+}
+
 interface Team {
   name: string;
   meta: Meta;
+  members: Member[];
 }
