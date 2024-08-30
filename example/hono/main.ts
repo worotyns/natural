4bbac4cf6569c22ff295793659d197efdb81cb2a
Mateusz Wu <15673167+worotyns@users.noreply.github.com>
2024-08-30 08:59:58 +0200

    comments

--- example/hono/main.ts
+++ example/hono/main.ts
@@ -3,6 +3,14 @@ import { app as authApp } from "./auth.ts";
 import { app as userApp } from "./user.ts";
 import { app as adminApp } from "./admin.ts";
 
+// TODO ATOMS:
+// Przekminic czy jeden atom nie powinnien miec "sztywnego" activity pod katem zmian i logow?
+// A dodatkowo robic wpisy w ns://acitivity/:ulid/{type} -> ns://users/sample@user.com/activity <- jest wpis i przywrocic w logach date :D 
+// Po czasie bym krecil sobie logami po prostu i je jakos czyscil albo archiwizowal? (team / user) jest problemem, reszta jest git bo to procesowe podejscia,
+// Albo robic "dzienne/tgodniowe/miesieczne/roczne/singleton" wpisy po prostu w zaleznosci od potrzeby?
+// Na atomie zrobic po prostu {activity: {nsid, pattern, current}}, tak zeby rotacja zadzialałała
+
+
 // TODO:
 // Prepare admin "scan" activites endpoint
 // Create a team
