1f3316408782a3ae57be8063dcf35041e0dd1cbe
Mateusz Wu <15673167+worotyns@users.noreply.github.com>
2024-09-02 12:18:06 +0200

    fix

--- testing.ts
+++ testing.ts
@@ -6,4 +6,4 @@ export {
 } from "@std/assert";
 
 export { spy, stub } from "@std/testing/mock";
-export { clearStorage, dumpStorage } from "./repository.ts" 
+export { clearStorage, dumpStorage } from "./repository.ts";
