d72a4ee56e5e9f71f312994e00e94847e17d05aa
Mateusz Wu <15673167+worotyns@users.noreply.github.com>
2024-09-22 20:07:46 +0200

    feat: dash

--- dashboard/src/index.ts
+++ dashboard/src/index.ts
@@ -2,5 +2,22 @@ import "./styles.css";
 import Alpine from "alpinejs";
 // import PineconeRouter from 'pinecone-router';
 window.Alpine = Alpine;
+
 // Alpine.plugin(PineconeRouter);
+Alpine.store("main", {
+  searchQuery: '',
+  selectedTab: 'Liked',
+  commands: [
+      { name: 'Command Name 1', description: 'Description of command 1.', parameters: ['Date', 'Boolean', 'String', 'Number'], expanded: false },
+      { name: 'Command Name 2', description: 'Description of command 2.', parameters: ['Parameter A', 'Parameter B'], expanded: false },
+      { name: 'Command Name 3', description: 'Description of command 3.', parameters: ['Parameter X', 'Parameter Y'], expanded: false }
+  ],
+  items: Array.from({ length: 30 }, (_, i) => ({
+      date: new Date().toLocaleDateString(),
+      title: `Title ${i + 1}`,
+      description: `Description for item ${i + 1}`,
+      liked: false
+  }))
+})
+
 Alpine.start();
